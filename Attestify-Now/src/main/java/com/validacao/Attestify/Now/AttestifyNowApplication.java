43383490a58a68881f09ff085b95c232c72a64f1
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-04-01 20:40:11 -0300

    Adição de arquivos para upload

---
+++ Attestify-Now/src/main/java/com/validacao/Attestify/Now/AttestifyNowApplication.java
@@ -0,0 +1,13 @@
+package com.validacao.Attestify.Now;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttestifyNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttestifyNowApplication.class, args);
+	}
+
+}
---
+++ Attestify-Now/src/test/java/com/validacao/Attestify/Now/AttestifyNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validacao.Attestify.Now;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttestifyNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
