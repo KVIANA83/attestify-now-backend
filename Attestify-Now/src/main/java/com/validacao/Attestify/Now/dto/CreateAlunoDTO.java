118f0c67aa37abb9cee9cb88b4ec540de6f1e023
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-04-04 20:19:21 -0300

    Adição de arquivos para upload

---
+++ Attestify-Now/src/main/java/com/validacao/Attestify/Now/dto/CreateAlunoDTO.java
@@ -0,0 +1,77 @@
+package com.validacao.Attestify.Now.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateAlunoDTO {
+    
+    @NotBlank(message = "Nome completo é um campo obrigatório!")
+    private String nomeCompleto;
+    
+    @NotNull(message = "Data de nascimento é um campo obrigatório!")
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate dataNascimento;
+    
+    @NotBlank(message = "Endereço é um campo obrigatório!")
+    private String endereco;
+    
+    @NotBlank(message = "sexo é um campo obrigatório!")
+    private String sexo;
+    
+    @NotBlank(message = "RG é um campo obrigatório!")
+    private String rg;
+    
+    @NotNull(message = "CPF é um campo obrigatório!")
+    private Long cpf;
+    
+    @NotBlank(message = "Nome do responsável é um campo obrigatório!")
+    private String nomeResponsavel;
+    
+    @NotBlank(message = "Grau de parentesco é um campo obrigatório!")
+    private String grauParentesco;
+    
+    @NotNull(message = "Data de nascimento do responsável é um campo obrigatório!")
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate dataNascimentoResponsavel;
+    
+    @NotBlank(message = "Telefone é um campo obrigatório!")
+    private String contato;
+    
+    @NotBlank(message = "E-mail é um campo obrigatório!")
+    @Email(message = "")
+    private String email;
+    
+    @NotBlank(message = "Criar login é um campo obrigatório!")
+    private String login;
+    
+    @NotBlank(message = "Criar senha é um campo obrigatório!")
+    private String senha;
+    
+    @NotBlank(message = "Tipo de perfil é um campo obrigatório!")
+    private String perfilLogin;
+    
+    @NotBlank(message = "Instituição é um campo obrigatório!")
+    private String instituicao;
+    
+    @NotBlank(message = "Modalidade é um campo obrigatório!")
+    private String modalidade;
+    
+    @NotBlank(message = "Turma é um campo obrigatório!")
+    private String turma;
+    
+    @NotBlank(message = "Turno é um campo obrigatório!")
+    private String turno;
+}
---
+++ Attestify-Now/src/main/java/com/validacao/Attestify/Now/dto/CreateAtestadoDTO.java
@@ -0,0 +1,34 @@
+package com.validacao.Attestify.Now.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateAtestadoDTO {
+    
+    @NotBlank(message = "Arquivo é um campo obrigatório!")
+    private String urlArquivo;
+    
+    // Data que o atestado foi impresso pelo prestador de serviço de saúde
+    @NotNull(message = "Data de Solicitação é um campo obrigatório!")
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate dataSolicitacao;
+    
+    // Data que o arquivo foi anexado no Attestify Now para realizar a entrega
+    @NotNull(message = "Data de Entrega é um campo obrigatório!")
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate dataEntrega;
+    
+}
