27719982758d210d08e38ebe637fc885628d65a7
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-05-10 23:27:50 -0300

    Update AlunoRepository.java

--- Attestify-Now/src/main/java/com/validacao/Attestify/Now/repository/AlunoRepository.java
+++ Attestify-Now/src/main/java/com/validacao/Attestify/Now/repository/AlunoRepository.java
@@ -1,5 +1,18 @@
 package com.validacao.Attestify.Now.repositories;
 
-public interface AlunoRepository {
+import com.validacao.Attestify.Now.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+    
+    Optional<Aluno> findByNomeCompleto(String nomeCompleto);
+    
+    Optional<Aluno> findByTurma(String turma);
+    
+    Optional<Aluno> findByCurso(String curso);
     
 }
