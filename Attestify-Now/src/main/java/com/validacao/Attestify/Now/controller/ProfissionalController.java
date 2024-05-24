60a37d1e05041d16b7331eb1100055ffcda3ac75
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-05-24 13:53:03 -0300

    Update ProfissionalController.java

--- Attestify-Now/src/main/java/com/validacao/Attestify/Now/controller/ProfissionalController.java
+++ Attestify-Now/src/main/java/com/validacao/Attestify/Now/controller/ProfissionalController.java
@@ -1,1 +1,63 @@
+package com.validacao.Attestify.Now.controller;
+
+import com.validacao.Attestify.Now.dto.CreateProfissionalDTO;
+import com.validacao.Attestify.Now.dto.ProfissionalDTO;
+import com.validacao.Attestify.Now.model.Profissional;
+import com.validacao.Attestify.Now.service.ProfissionalService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/profissional")
+public class ProfissionalController {
+    
+    private ProfissionalService profissionalService;
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<ProfissionalDTO>> listarProfissionais() {
+        List<ProfissionalDTO> profissionais = profissionalService.listarTodos();
+        return ResponseEntity.ok(profissionais);
+    }
+
+    @GetMapping("/buscar/id/{id}")
+    public ResponseEntity<ProfissionalDTO> getProfissionalPorId(@PathVariable Long id) {
+        ProfissionalDTO profissional = profissionalService.pegarProfissionalPeloId(id);
+        return ResponseEntity.ok(profissional);
+    }
+
+    @GetMapping("/buscar/nome/{nome}")
+    public ResponseEntity<List<ProfissionalDTO>> getProfissionalPorNome(@PathVariable String nome) {
+        List<ProfissionalDTO> profissionais = profissionalService.pegarProfissionalPeloNome(nome);
+        return ResponseEntity.ok(profissionais);
+    }
+
+    @GetMapping("/buscar/cargo/{cargo}")
+    public ResponseEntity<List<ProfissionalDTO>> getProfissionalPorCargo(@PathVariable String cargo) {
+        List<ProfissionalDTO> profissionais = profissionalService.pegarProfissionalPorCargo(cargo);
+        return ResponseEntity.ok(profissionais);
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<Void> criarProfissional(@Valid @RequestBody CreateProfissionalDTO profissionalDTO) {
+        profissionalService.saveProfissional(profissionalDTO);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/atualizar/{id}")
+    public ResponseEntity<Void> atualizarProfissional(@PathVariable Long id, @Valid @RequestBody CreateProfissionalDTO profissionalAtualizar) {
+        profissionalService.atualizarProfissional(profissionalAtualizar, id);
+        return ResponseEntity.noContent().build();
+    }
+    
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Void> deletarProfissional(@PathVariable Long id) {
+        profissionalService.deletarProfissional(id);
+        return ResponseEntity.noContent().build();
+    }
+}
 
