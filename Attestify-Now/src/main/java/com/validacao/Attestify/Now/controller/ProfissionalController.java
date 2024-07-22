7e493b6b4b03c3520bb6985cae2e1cb08253acd1
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-07-22 14:14:44 -0300

    Atualizar o ProfissionalController.java
    update

--- Attestify-Now/src/main/java/com/validacao/Attestify/Now/controller/ProfissionalController.java
+++ Attestify-Now/src/main/java/com/validacao/Attestify/Now/controller/ProfissionalController.java
@@ -18,46 +18,52 @@ public class ProfissionalController {
     
     private ProfissionalService profissionalService;
 
+    // Endpoint para listar todos os profissionais
     @GetMapping("/listar")
     public ResponseEntity<List<ProfissionalDTO>> listarProfissionais() {
         List<ProfissionalDTO> profissionais = profissionalService.listarTodos();
         return ResponseEntity.ok(profissionais);
     }
 
+    // Endpoint para buscar um profissional por ID
     @GetMapping("/buscar/id/{id}")
     public ResponseEntity<ProfissionalDTO> getProfissionalPorId(@PathVariable Long id) {
         ProfissionalDTO profissional = profissionalService.pegarProfissionalPeloId(id);
         return ResponseEntity.ok(profissional);
     }
 
+    // Endpoint para buscar profissionais por nome
     @GetMapping("/buscar/nome/{nome}")
     public ResponseEntity<List<ProfissionalDTO>> getProfissionalPorNome(@PathVariable String nome) {
         List<ProfissionalDTO> profissionais = profissionalService.pegarProfissionalPeloNome(nome);
         return ResponseEntity.ok(profissionais);
     }
 
+    // Endpoint para buscar profissionais por cargo
     @GetMapping("/buscar/cargo/{cargo}")
     public ResponseEntity<List<ProfissionalDTO>> getProfissionalPorCargo(@PathVariable String cargo) {
         List<ProfissionalDTO> profissionais = profissionalService.pegarProfissionalPorCargo(cargo);
         return ResponseEntity.ok(profissionais);
     }
 
+    // Endpoint para criar um novo profissional
     @PostMapping("/criar")
     public ResponseEntity<Void> criarProfissional(@Valid @RequestBody CreateProfissionalDTO profissionalDTO) {
         profissionalService.saveProfissional(profissionalDTO);
         return ResponseEntity.noContent().build();
     }
 
+    // Endpoint para atualizar um profissional existente
     @PostMapping("/atualizar/{id}")
     public ResponseEntity<Void> atualizarProfissional(@PathVariable Long id, @Valid @RequestBody CreateProfissionalDTO profissionalAtualizar) {
         profissionalService.atualizarProfissional(profissionalAtualizar, id);
         return ResponseEntity.noContent().build();
     }
     
+    // Endpoint para deletar profissional por id
     @DeleteMapping("/deletar/{id}")
     public ResponseEntity<Void> deletarProfissional(@PathVariable Long id) {
         profissionalService.deletarProfissional(id);
         return ResponseEntity.noContent().build();
     }
 }
-
